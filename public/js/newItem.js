8cca664efb31149e1696b83d31c92da09f46153f
Andrew Hall <andrewbhall96@gmail.com>
2020-07-19 14:53:49 -0400

    newItem.js makes a new Item object, logs data, sends to server

--- public/js/newItem.js
+++ public/js/newItem.js
@@ -1,28 +1,45 @@
 $("#add-item").on("click", function(event) {
     event.preventDefault();
-    // make a newCharacter obj
+    // make a new Item obj
     var newItem = {
       // name from name input
-      name: $("#name").val().trim(),
-      // role from role input
-      role: $("#role").val().trim(),
-      // age from age input
-      age: $("#age").val().trim(),
-      // points from force-points input
-      forcePoints: $("#force-points").val().trim()
+      firstName: $("#firstName").val().trim(),
+      // name from name input
+      lastName: $("#lastName").val().trim(),
+      // email from email input
+      email: $("#email").val().trim(),
+      // category from category input
+      category: $("#category").val().trim(),
+    //   title from title imput
+      title: $("#title").val().trim(),
+      // price from price input
+      price: $("#price").val().trim(),
+      // condition from condition input
+      condition: $("#condition").val().trim(),
+      // link from link input
+      link: $("#link").val().trim(),
+      // description from description input
+      description: $("#description").val().trim()
+
     };
     // send an AJAX POST-request with jQuery
-    $.post("/api/new", newItem)
+    $.post("/api/item", newItem)
       // on success, run this callback
       .then(function(data) {
         // log the data we found
         console.log(data);
-        // tell the user we're adding a character with an alert window
-        alert("Adding character...");
+        // tell the user we're adding a new item with an alert window
+        alert("Adding new item...");
       });
     // empty each input box by replacing the value with an empty string
-    $("#name").val("");
-    $("#role").val("");
-    $("#age").val("");
-    $("#force-points").val("");
+    $("#firstName").val("");
+    $("#lastName").val("");
+    $("#email").val("");
+    $("#category").val("");
+    $("#title").val("");
+    $("#price").val("");
+    $("#condition").val("");
+    $("#link").val("");
+    $("#description").val("");
+
   });
