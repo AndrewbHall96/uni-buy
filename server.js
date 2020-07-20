e661c9471a4828211c12f281a3300b1af4b01d55
asharma1398 <asharma1398@gmail.com>
2020-07-20 10:56:41 -0400

    connections added to server.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // Dependencies
 var express = require("express");
+var exphbs = require("express-handlebars");
+require("dotenv").config();
 
 // Sets up the Express App
 var app = express();
@@ -25,7 +27,8 @@ app.engine(
 app.set("view engine", "handlebars");
 
 // Routes - more names to come 
-require("./routes/html-routes.js")(app);
+require("./routes/htmlRoutes.js")(app);
+require("./routes/apiRoutes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 db.sequelize.sync({ force: true }).then(function() {
