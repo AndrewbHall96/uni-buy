cb9fcc9fc540c1ea5fd6581a3f2124ff4feec6ee
Andrew Hall <andrewbhall96@gmail.com>
2020-07-22 14:34:45 -0400

    heroku deployment

--- config/config.js
+++ config/config.js
@@ -20,11 +20,6 @@ module.exports = {
   },
   "production": {
     "use_env_variable": "JAWSDB_URL",
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "port": 3306,
     "dialect": "mysql"
   }
 }
