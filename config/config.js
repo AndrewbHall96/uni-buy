b11e78365882bd341461c0630df2c4fbed7ba0e9
Andrew Hall <andrewbhall96@gmail.com>
2020-07-22 10:41:15 -0400

    resolved

--- config/config.js
+++ config/config.js
@@ -3,11 +3,7 @@ require("dotenv").config();
 module.exports = {
   "development": {
     "username": "root",
-<<<<<<< HEAD
     "password": "Cookie123",
-=======
-    "password": process.env.MY_SQL,
->>>>>>> c37156b72e983e4e70ed858d825df1bde6dbabd9
     "database": "sellerInfo",
     "host": "127.0.0.1",
     "port": 3306,
