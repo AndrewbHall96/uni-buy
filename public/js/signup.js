8ba2597b63345add0bdfd8df0bf4ef12714bd257
Gary Bergman <garybergman00@gmail.com>
2020-07-22 11:09:41 -0400

    Changed routes for button click

--- public/js/signup.js
+++ public/js/signup.js
@@ -42,7 +42,7 @@ $(document).ready(function() {
       password: password
     })
       .then(function(data) {
-        window.location.replace("/");
+        window.location.replace("/index");
         // If there's an error, handle it by throwing up a bootstrap alert
       })
       .catch(handleLoginErr);
